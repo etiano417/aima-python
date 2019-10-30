3704ddbce557b79f0339596924b7af87ffb748b4
Eric Tiano <etiano417@gmail.com>
2019-10-30 16:35:30 -0400

    Stubbed minmax_utility_label()
     - added a function call to tic-tac-toe_graph.py which produces a text label for a given game and state
     - added a func stub for tic-tac-toe_graph.py

--- gui/tic-tac-toe_graph.py
+++ gui/tic-tac-toe_graph.py
@@ -43,8 +43,8 @@ class MinMaxGraph:
             x = self.terminal_node_count * 25
             
         y = row*45
-        
-        #self.expand_canvas(x,y)
+
+        state_text = minmax_utility_label(state, self.game);    
         state_text = tic_tac_toe_state_text(state, self.game)
         self.canvas.create_text((x,y), text = state_text)
         self.canvas.pack()
@@ -67,11 +67,19 @@ translates the state of a tic-tac-toe game into a string
 """
 def tic_tac_toe_state_text(state, game):
     output = ""
-
     board = state.board
+    
     for x in range(1, game.h + 1):
         for y in range(1, game.v + 1):
             output += board.get((x, y), '#')
         output += "\n"
     return output
-        
+      
+"""
+returns a text string indicating the min/max utility of a given node
+"""
+def minmax_utility_label(state, game):
+    return("")
+    
+
+
